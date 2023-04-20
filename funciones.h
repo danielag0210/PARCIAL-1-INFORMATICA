2c4d991c6313e7de80c0f00e4a2ff9ff9c829d16
juliancardenasca <125225254+juliancardenasca@users.noreply.github.com>
2023-04-20 17:07:18 -0500

    Presenta fallas aun

--- funciones.h
+++ funciones.h
@@ -3,8 +3,6 @@
 
 #include <iostream>
 #include <fstream>
-
-/* Estas 3 librerias son solo para actualizar la consola */
 #include <cstdlib>
 #include <chrono>
 #include <thread>
@@ -12,13 +10,12 @@
 using namespace std;
 
 unsigned long long cursor();
-char* copiartxt(unsigned long long end1);
+char* leer(unsigned long long end1);
 char*** crearMatriz(int n, int m, int d, char* txt);
 void limpiar(int seconds);
 void menuHorario();
-char** Gestionar(int n);
+char ***Gestionar(int filas,int h1);
+int preguntar(char ***horario, int n);
 int contarSaltosDeLinea(char* arreglo);
-void imprimirMatriz(char*** matriz, int filas, int columnas);
-int preguntar(char*** matriz, int n);
 
 #endif // FUNCIONES_H
